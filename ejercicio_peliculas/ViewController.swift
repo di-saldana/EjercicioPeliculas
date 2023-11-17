3be6d538cb97cac26196c1df332ea34da7231242
di-saldana <dsl42@alu.ua.es>
2023-11-17 18:40:43 +0100

    Initial Commit

---
+++ ejercicio_peliculas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ejercicio_peliculas
+//
+//  Created by Dianelys Saldaña on 11/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
